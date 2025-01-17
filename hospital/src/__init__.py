3817f00656165d86b3e7e8977b19ab0ca56e2281
irsCooper <spock187sheldon@gmail.com>
2025-01-17 21:12:47 +0300

    add alembic and create models

---
+++ hospital/src/__init__.py
@@ -0,0 +1,7 @@
+__all__ = (
+    "RoomModel",
+    "HospitalModel",
+    "HospitalRoomsModel"
+)
+
+from src.hospitals.model import HospitalModel, HospitalRoomsModel, RoomModel
--- hospital/src/hospitals/model.py
+++ hospital/src/hospitals/model.py
@@ -1,2 +1,40 @@
+import uuid
+from sqlalchemy import ForeignKey, Integer, String
+from ..base_model import BaseModel
 
-# class RoomModel()
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+
+class HospitalRoomsModel(BaseModel):
+    __tablename__ = 'hospital_rooms'
+
+    hospital_id: Mapped[int] = mapped_column(ForeignKey("hospitals.id", ondelete="CASCADE"), primary_key=True)
+    room_id: Mapped[int] = mapped_column(ForeignKey("rooms.id",  ondelete="CASCADE"), primary_key=True)
+
+
+class RoomModel(BaseModel):
+    __tablename__ = 'rooms'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, default=uuid.uuid4)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+
+    hospitals: Mapped[list["HospitalModel"]] = relationship(
+        "HospitalModel",
+        secondary="hospital_rooms",
+        back_populates="rooms"
+    )
+
+
+class HospitalModel(BaseModel):
+    __tablename__ = 'hospitals'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, default=uuid.uuid4)
+    name: Mapped[str] = mapped_column(String, nullable=False, unique=True)
+    address: Mapped[str] = mapped_column(String, nullable=False)
+    contactPhone: Mapped[str] = mapped_column(String(300), nullable=False, unique=True)
+
+    rooms: Mapped[list["RoomModel"]] = relationship(
+        "RoomModel",
+        secondary="hospital_rooms",
+        back_populates="hospitals",
+    )
