ffee48fd09ddf37f9796d47130d4b24ea7d2e30c
irsCooper <spock187sheldon@gmail.com>
2025-01-18 23:57:50 +0300

    ended base functional

--- hospital/main.py
+++ hospital/main.py
@@ -1,6 +1,8 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from src.hospitals.router import router as router_hospital
+
 import uvicorn
 
 
@@ -8,11 +10,15 @@ import uvicorn
 async def lifespan(app: FastAPI):
     yield
 
+
+
 app = FastAPI(
     title="Hospital Microsevices",
     docs_url="/ui-swagger",
     lifespan=lifespan
 )
+
+app.include_router(router_hospital)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8082, reload=True)
---
+++ hospital/src/hospitals/router.py
@@ -0,0 +1,81 @@
+import uuid
+from fastapi import APIRouter, Depends, Query
+
+from src.hospitals.service import HospitalService
+from src.hospitals.schemas import HospitalCreate, HospitalSchema, HospitalUpdate, RoomSchema
+from src.core.db_helper import db
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+router =APIRouter(
+    prefix="/Hospitals",
+    tags=["Hospitals"]
+)
+
+@router.get("", response_model_exclude=list[HospitalSchema])
+async def get_list_hospitals(
+    offset: int = Query(..., alias='from', ge=0),
+    limit: int = Query(..., alias='count', ge=1),
+    session: AsyncSession = Depends(db.session_dependency)
+):
+    return await HospitalService.get_hospitals(
+        offset=offset,
+        limit=limit,
+        session=session
+    )
+
+
+@router.get("/{id}", response_model_exclude=HospitalSchema)
+async def get_hospital_by_id(
+    id: uuid.UUID,
+    session: AsyncSession = Depends(db.session_dependency)
+):
+    return await HospitalService.get_hospital(
+        hospital_id=id,
+        session=session
+    )
+
+
+@router.get("/{id}/Rooms", response_model_exclude=RoomSchema)
+async def get_romms_in_hospital_by_id(
+    id: uuid.UUID,
+    session: AsyncSession = Depends(db.session_dependency)
+):
+    return await HospitalService.get_list_rooms(
+        hospital_id=id,
+        session=session
+    )
+
+
+@router.post("", response_model=HospitalSchema)
+async def create_hospital_only_admin(
+    data: HospitalCreate,
+    session: AsyncSession = Depends(db.session_dependency)
+):
+    return await HospitalService.create_hospital(
+        data=data,
+        session=session
+    ) 
+
+
+@router.put("/{id}")
+async def update_hospital_info_by_id_only_admin(
+    data: HospitalUpdate,
+    session: AsyncSession = Depends(db.session_dependency)
+):
+    return await HospitalService.update_hospital(
+        hospital_id=data.id,
+        data=data,
+        session=session
+    ) 
+
+
+@router.delete("/{id}")
+async def delete_hospital_by_id_only_admin(
+    id: uuid.UUID,
+    session: AsyncSession = Depends(db.session_dependency)
+):
+    return await HospitalService.delete_hospital(
+        hospital_id=id,
+        session=session
+    )
