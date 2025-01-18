6e5378f67257f8939e1ea1e8c40bdb301be91bf4
irsCooper <spock187sheldon@gmail.com>
2025-01-18 16:41:48 +0300

    add new exception

--- hospital/src/exception/DatabaseException.py
+++ hospital/src/exception/DatabaseException.py
@@ -13,3 +13,11 @@ class UnknowanDatabaseException(HTTPException):
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="Unknowan Exception: Cannot insert data into table"
         )
+
+
+class ConflictUniqueAttribute(HTTPException):
+    def __init__(self, detail):
+        super().__init__(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"Conflict Exc: {detail}"
+        )
