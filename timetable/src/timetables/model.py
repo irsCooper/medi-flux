f546914e077fb7a5dd01b8e69295ace7f9140f4a
irsCooper <spock187sheldon@gmail.com>
2025-02-06 01:27:27 +0300

    create model timetable

---
+++ timetable/src/timetables/model.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+import uuid
+
+from sqlalchemy import TIMESTAMP, UUID, String
+from src.base_model import BaseModel
+
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class Timetable(BaseModel):
+    __tablename__ = 'timetable'
+
+    id: Mapped[uuid.UUID] = mapped_column(UUID, primary_key=True, default=uuid.uuid4)
+    hospital_id: Mapped[uuid.UUID] = mapped_column(UUID, index=True, nullable=False)
+    doctor_id: Mapped[uuid.UUID] = mapped_column(UUID, index=True, nullable=False)
+    from_column: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), nullable=False)
+    to: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), nullable=False)
+    room: Mapped[str] = mapped_column(String, nullable=False)
