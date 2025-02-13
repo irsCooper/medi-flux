189d27e128d0de945d01688d210fbe87d0250ccc
irsCooper <spock187sheldon@gmail.com>
2025-02-13 10:25:07 +0000

    add rabbit to main.py

--- hospital/main.py
+++ hospital/main.py
@@ -4,13 +4,19 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from src.hospitals.router import router as router_hospital
+from src.rabbit_mq.server import consume_rabbitmq
 
 import uvicorn
+import asyncio
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    yield
+    task = asyncio.create_task(consume_rabbitmq())
+    try: 
+        yield
+    finally:
+        task.cancel()
 
 
 
