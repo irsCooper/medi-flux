bb592b348bf7c6c9378f2dfcd129c45444e608dc
irsCooper <spock187sheldon@gmail.com>
2025-02-28 23:02:47 +0300

    add base rabbit_mq

---
+++ document/src/rabbit_mq/server.py
@@ -0,0 +1,16 @@
+import asyncio
+
+from aio_pika import connect_robust
+
+from src.core.db_helper import db
+from src.core.config import settings
+
+
+async def consume_rabbitmq():
+        try:
+            await connect_robust(settings.rabbit_mq_url)
+
+            print('Успешное подключение к RabbitMQ')
+        except Exception as e:
+            print(f'Ошибка подключения к RabbitMQ: {e}. Переподключение через 5 секунд...')
+            await asyncio.sleep(5)
